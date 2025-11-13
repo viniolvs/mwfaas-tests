de663b0cc58aaf11c3ea4228396327b461984a79
Vinicius de Oliveira Silva <viniolvs.dev@gmail.com>
2025-11-13 19:20:43 -0300

    analise script

---
+++ analise.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# Script para Contagem de Tarefas por Worker
+# Recebe o nome do arquivo como argumento da linha de comando.
+#
+# Exemplo de uso:
+# ./contar_workers.sh gzip_1.txt
+#
+
+# --- Validação do Argumento ---
+# Verifica se um argumento (nome do arquivo) foi fornecido
+if [ -z "$1" ]; then
+    echo "Erro: Você precisa fornecer o nome do arquivo como argumento." >&2
+    echo "Exemplo de uso: $0 nome_do_arquivo.txt" >&2
+    exit 1
+fi
+
+# Usa o primeiro argumento da linha de comando ($1) como o nome do arquivo
+FILENAME=$1
+
+# Verifica se o arquivo existe
+if [ ! -f "$FILENAME" ]; then
+    echo "Erro: Arquivo '$FILENAME' não encontrado." >&2
+    exit 1
+fi
+
+echo "Analisando o arquivo: $FILENAME"
+
+echo "-------------------------------------"
+
+echo "[Master] $(grep -E "master.run()" $FILENAME)"
+echo "$(grep -E "Master" $FILENAME | grep -v -E "Execution times")"
+
+# --- Execução da Contagem ---
+echo "-------------------------------------"
+echo "Tarefas executadas por Worker lab_1, lab_2, lab_3, lab_4, lab_5: "
+echo "$(grep -c -E "c5d1ef1f-76c0-40e0-a9b0-e1cfbd95ecd8.+completou" $FILENAME)" # lab_1
+echo "$(grep -c -E "d93b465f-d252-4574-8946-9ea53e5cdd6e.+completou" $FILENAME)" # lab_2
+echo "$(grep -c -E "814c4221-8c4a-45d4-b70f-379fd28d5ef9.+completou" $FILENAME)" # lab_3
+echo "$(grep -c -E "9b55bf7c-c552-451e-b424-174e59d8ad50.+completou" $FILENAME)" # lab_4
+echo "$(grep -c -E "e0888edc-7356-4a27-91bc-936113288f10.+completou" $FILENAME)" # lab_5
